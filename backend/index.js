0001b4302c7e715fb6d1c56c7ec180a8152fd6c7
shashwat558 <tumbaktuhere@gmail.com>
2024-05-26 11:56:32 +0530

    h

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const mainRouter = require('./router/index')
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.use('api/v1', mainRouter)
+
+app.listen(3000)
+
