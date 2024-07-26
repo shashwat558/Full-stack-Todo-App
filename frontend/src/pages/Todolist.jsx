51455f0f5fa1a3f17157e46b8bb4beb85b0f5026
shashwat558 <tumbaktuhere@gmail.com>
2024-07-26 23:29:46 +0530

    not much changes today

--- frontend/src/pages/Todolist.jsx
+++ frontend/src/pages/Todolist.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const Todolist = () => {
+    const [todos, setTodos] = useState([]);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    useEffect(() => {
+        const getTodos = async () => {
+            const response = await axios.get("http://localhost:3000/api/todo/todos", {
+              headers: {Authorization:`Bearer ${localStorage.getItem("token")}`}
+            })
+        }
+    })
   return (
-    <div>Todolist</div>
+    <div></div>
   )
 }
 
