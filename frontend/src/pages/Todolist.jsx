f34382348292f7e2252f2bea28135a619d3db32f
shashwat558 <tumbaktuhere@gmail.com>
2024-08-02 01:29:25 +0530

    coded some todo functionlity

--- frontend/src/pages/Todolist.jsx
+++ frontend/src/pages/Todolist.jsx
@@ -4,9 +4,12 @@ import Context from '../Context/Context';
 
 const Todolist = () => {
     const [todos, setTodos] = useState([]);
+  
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const {user} = useContext(Context)
+
+    
 
     useEffect(() => {
         const getTodos = async () => {
@@ -21,24 +24,33 @@ const Todolist = () => {
     },[])
 
     const addTask = async () => {
+      const responce = await axios.post("http://localhost:3000/api/todo/todos",{title, description})
       
     }
   return (
     <div>
       {user.name}
       {todos.map((todo) => {
+        
         <div>
           <div>{todo.id}</div>
         <div>{todo.title}</div>
         <div>{todo.description}</div>
         <button type=''></button>
         </div>
-
+      
       })}
+      <div>
+        <input type="text" value={title} onChange={(e) => {setTitle(e.target.value)}}/>
+        <input type="text" value={description} onChange={(e) => setDescription(e.target.value)}/>
+        <button onclick>Add task</button>
+      </div>
 
 
     </div>
   )
+
+
 }
 
 export default Todolist
